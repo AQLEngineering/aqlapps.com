52d594e0081050e8608dc384076314bedb37766c
AQL Engineering, lda <geral@aqlengineering.com>
2026-08-06 12:37:44 +0100

    Deploy aqlapps.com

---
+++ assets/client-CF7W_g-w.js
@@ -0,0 +1,1 @@
+import{c as s}from"./index-BAOvnebo.js";import{i as a,J as o}from"./index-DUpwX2ah.js";const t=`https://${o}.supabase.co`,r=s(t,a);export{r as s};
---
+++ assets/voice-textarea-CGhjQP27.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-DUpwX2ah.js";import{T as a}from"./textarea-DlzcHusL.js";function i({value:r,onValueChange:t,...e}){return o.jsx(a,{...e,value:r,onValueChange:t})}export{i as V};
