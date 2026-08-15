c524e4a8d49dfbbad5d2ea732b4e44c41a83a6bd
AQL Engineering, lda <geral@aqlengineering.com>
2026-08-15 19:38:52 +0100

    Deploy aqlapps.com

--- assets/index-BKmcsEKs.js
+++
@@ -1,1 +0,0 @@
-import{x as r}from"./index-Js2SPJ3Q.js";import{r as o}from"./index-Chjiymov.js";var p=o();const t=r(p);export{t as P};
---
+++ assets/voice-textarea-wGzNdz6o.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-BqqVEm15.js";import{T as a}from"./textarea-CuJr6ocx.js";function i({value:r,onValueChange:t,...e}){return o.jsx(a,{...e,value:r,onValueChange:t})}export{i as V};
