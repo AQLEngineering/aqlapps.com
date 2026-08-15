b2be5d4a0a6ce49a6205d20a2fcabc6932c7d67f
AQL Engineering, lda <geral@aqlengineering.com>
2026-08-15 16:47:21 +0100

    Deploy aqlapps.com

---
+++ assets/voice-textarea-Bbft0O_P.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-BiAvrPgf.js";import{T as a}from"./textarea-BQrVPQvj.js";function i({value:r,onValueChange:t,...e}){return o.jsx(a,{...e,value:r,onValueChange:t})}export{i as V};
