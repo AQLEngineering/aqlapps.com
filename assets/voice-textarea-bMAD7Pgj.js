9c0e13e5bda601a697c1503e4a8fa7b7daa48841
AQL Engineering, lda <geral@aqlengineering.com>
2026-08-28 21:29:57 +0100

    Deploy aqlapps.com

---
+++ assets/voice-textarea-bMAD7Pgj.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-c0_r_63b.js";import{T as a}from"./textarea-HwE3kNjF.js";function i({value:r,onValueChange:t,...e}){return o.jsx(a,{...e,value:r,onValueChange:t})}export{i as V};
