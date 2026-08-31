194cd25c1797bf740973c9379a596059e3d990f7
AQL Engineering, lda <geral@aqlengineering.com>
2026-08-31 16:59:27 +0100

    Revert "Deploy aqlapps.com"
    This reverts commit 62cfb201ad2f5d5aec7895fc344c909110a7e2a9.

--- assets/voice-textarea-Kam3i-FE.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-C4Ajcm92.js";import{T as a}from"./textarea-Bts4cTRD.js";function i({value:r,onValueChange:t,...e}){return o.jsx(a,{...e,value:r,onValueChange:t})}export{i as V};
