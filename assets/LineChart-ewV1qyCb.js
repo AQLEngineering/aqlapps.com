9589ee46125cba9e94ef9608ff86bb40cc302d63
AQL Engineering, lda <geral@aqlengineering.com>
2026-08-31 17:01:16 +0100

    Restore AQL Fisio 1.13.7 production build

--- assets/LineChart-ewV1qyCb.js
+++
@@ -1,1 +0,0 @@
-import{g as a,X as i,Y as s,f as r}from"./generateCategoricalChart-do9wrVce.js";import{L as x}from"./Line-B2e1sQQf.js";var t=a({chartName:"LineChart",GraphicalChild:x,axisComponents:[{axisType:"xAxis",AxisComp:i},{axisType:"yAxis",AxisComp:s}],formatAxisMap:r});export{t as L};
--- assets/voice-textarea-BdCxXF76.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-CN2CuE1M.js";import{T as a}from"./textarea-oXaTp09d.js";function i({value:r,onValueChange:t,...e}){return o.jsx(a,{...e,value:r,onValueChange:t})}export{i as V};
