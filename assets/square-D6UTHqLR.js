f01a4d31452d52da921947bc287498c34caaf9e2
AQL Engineering, lda <geral@aqlengineering.com>
2026-09-12 10:55:56 +0100

    Deploy aqlapps.com

---
+++ assets/square-D6UTHqLR.js
@@ -0,0 +1,11 @@
+import{c as e}from"./index-DSaQAjkd.js";/**
+ * @license lucide-react v0.487.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=[["path",{d:"M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z",key:"1a8usu"}],["path",{d:"m15 5 4 4",key:"1mk7zo"}]],t=e("pencil",a);/**
+ * @license lucide-react v0.487.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["rect",{width:"18",height:"18",x:"3",y:"3",rx:"2",key:"afitv7"}]],i=e("square",c);export{t as P,i as S};
---
+++ assets/voice-textarea-C3g4OzLE.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-DSaQAjkd.js";import{T as a}from"./textarea-DVjKzqhq.js";function i({value:r,onValueChange:t,...e}){return o.jsx(a,{...e,value:r,onValueChange:t})}export{i as V};
