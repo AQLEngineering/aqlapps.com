82a68491630ff94649b7afa65a868195ac112245
AQL Engineering, lda <geral@aqlengineering.com>
2026-09-13 10:59:51 +0100

    Deploy aqlapps.com

---
+++ assets/client-D6wkdo_Y.js
@@ -0,0 +1,1 @@
+import{c as s}from"./index-Bc72485a.js";import{E as a,a2 as o}from"./index-hiCnJoRo.js";const t=`https://${o}.supabase.co`,r=s(t,a);export{r as s};
---
+++ assets/voice-textarea-D9Ax-1m7.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-hiCnJoRo.js";import{T as a}from"./textarea-f6YzNCU5.js";function i({value:r,onValueChange:t,...e}){return o.jsx(a,{...e,value:r,onValueChange:t})}export{i as V};
