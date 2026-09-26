94b39b4d6618ee084d47096d14a294f596c47143
AQL Engineering, lda <geral@aqlengineering.com>
2026-09-26 09:46:35 +0100

    Deploy aqlapps.com

---
+++ assets/client-CWHL3Tnr.js
@@ -0,0 +1,1 @@
+import{c as s}from"./index-D_-aS1of.js";import{a6 as a,aU as o}from"./index-CVoKCZld.js";const t=`https://${o}.supabase.co`,r=s(t,a);export{r as s};
---
+++ assets/index-IJ5AtuN1.js
@@ -0,0 +1,1 @@
+import{aL as r}from"./index-CVoKCZld.js";import{r as o}from"./index-Chjiymov.js";var p=o();const t=r(p);export{t as P};
